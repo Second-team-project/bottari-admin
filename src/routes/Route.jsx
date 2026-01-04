f33d0ebd1a6eb577c9ec8caef6ba3ded9145700d
underthebluemoon <underthebluemoon0@gmail.com>
2026-01-05 02:43:30 +0900

    260104 추가한 페이지 메뉴에 추가, 라우트 추가

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -5,7 +5,15 @@ import App from "../App.jsx";
 import Monitoring from "../components/monitoring/Monitoring.jsx";
 import Login from "../components/auth/Login.jsx";
 import ReservationList from "../components/reservation/ReservationList.jsx";
+import DriverList from "../components/drivers/DriverList.jsx";
+import EmployeeList from "../components/employees/EmployeeList.jsx";
 import AnnouncementAdmin from "../components/Announcement/AnnouncementAdmin.jsx";
+import NoticeList from "../components/notice/NoticeList.jsx";
+import FaqList from "../components/faq/FaqList.jsx";
+import PricingPage from "../components/pricing/PricingPage.jsx";
+import ImageManagePage from "../components/image/ImageManagePage.jsx";
+import StoreList from "../components/store/StoreList.jsx";
+import UserList from "../components/users/UserList.jsx";
 
 
 const router = createBrowserRouter([
@@ -27,8 +35,40 @@ const router = createBrowserRouter([
         element: <Monitoring />,
       },
       {
-        path: "/ReservationList",
+        path: "/reservations",
         element: <ReservationList />,
+      },
+      {
+        path: "/drivers",
+        element: <DriverList />,
+      },
+      {
+        path: "/employees",
+        element: <EmployeeList />,
+      },
+      {
+        path: "/users",
+        element: <UserList />,
+      },
+      {
+        path: "/notice",
+        element: <NoticeList />,
+      },
+      {
+        path: "/faq",
+        element: <FaqList />,
+      },
+      {
+        path: "/pricing",
+        element: <PricingPage />,
+      },
+      {
+        path: "/image",
+        element: <ImageManagePage />,
+      },
+      {
+        path: "/store",
+        element: <StoreList />,
       },
       {
         path: "/announcement",
